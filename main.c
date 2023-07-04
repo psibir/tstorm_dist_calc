713293e2b273f7f14ae1705df32ca019dde95508
Trevor Bloomfield <bloomfieldtm@gmail.com>
2023-07-04 12:23:41 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 
 #define SPEED_OF_SOUND 343.2
-#define INVALID_DISTANCE -1.0
-#define INVALID_INPUT -1.0
+#define SUCCESS 0
+#define ERROR_INVALID_INPUT -1
+#define ERROR_NEGATIVE_TIME -2
 
-double calculate_distance(double time_elapsed) {
+int validate_time_elapsed(double time_elapsed) {
     if (time_elapsed < 0) {
         printf("Invalid input: time elapsed cannot be negative.\n");
-        return INVALID_DISTANCE;
+        return ERROR_NEGATIVE_TIME;
     }
 
-    return SPEED_OF_SOUND * time_elapsed;
+    return SUCCESS;
 }
 
 double get_time_elapsed() {
     double time_elapsed;
-    printf("Enter the time elapsed (in seconds) between seeing the lightning and hearing the thunder: ");
 
-    if (scanf("%lf", &time_elapsed) != 1) {
-        printf("Error reading input. Please enter a valid floating-point number.\n");
-        return INVALID_INPUT;
+    while (1) {
+        printf("Enter the time elapsed (in seconds) between seeing the lightning and hearing the thunder: ");
+
+        if (scanf("%lf", &time_elapsed) != 1) {
+            printf("Error reading input. Please enter a valid floating-point number.\n");
+            return ERROR_INVALID_INPUT;
+        }
+
+        int validation_result = validate_time_elapsed(time_elapsed);
+        if (validation_result == SUCCESS) {
+            break;
+        } else if (validation_result == ERROR_NEGATIVE_TIME) {
+            continue;
+        }
     }
 
     return time_elapsed;
@@ -34,8 +45,10 @@ void print_distance(double distance) {
 int main() {
     double time_elapsed = get_time_elapsed();
 
-    double distance = calculate_distance(time_elapsed);
-    print_distance(distance);
+    if (time_elapsed != ERROR_INVALID_INPUT && time_elapsed != ERROR_NEGATIVE_TIME) {
+        double distance = SPEED_OF_SOUND * time_elapsed;
+        print_distance(distance);
+    }
 
     return 0;
 }
